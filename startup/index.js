6177267204e816f51f6f193ca9e84e9df306b8c9
Jeremy Bass <jeremybass@cableone.net>
2017-06-10 21:43:07 -0700

    make sure we include the features to choose from

--- startup/index.js
+++ startup/index.js
@@ -3,11 +3,11 @@ var util = require('util');
 var ScriptBase = require('../script-base-basic');
 
 var Generator = module.exports = function Generator() {
-  ScriptBase.apply(this, arguments);
+    ScriptBase.apply(this, arguments);
 };
 
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.createItem = function() {
-  this.generateTemplateFile('Startup.cs', 'Startup.cs', { namespace: this.namespace() });
+    this.generateTemplateFile('Startup.cs', 'Startup.cs', { namespace: this.namespace(), features: this.features });
 };
